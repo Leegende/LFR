47a1428c5ade758aacfb4c5fa1cabd0244a82163
Leegende <leqitknxckbackjr@gmail.com>
2025-01-12 10:58:38 +0100

    africa work 1

---
+++ statemerger.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+import re
+
+# first is goal
+toMergeSet = [227,84,82,229,86,91,226]
+toMerge = list(set(toMergeSet))
+
+path="./history/states/"
+
+listt = os.listdir(path)
+#listt.sort()
+
+arges = ["java", "-jar", "pp.jar", "fotzenwolf.lua"]
+
+def tryParse() -> str: 
+    ret = subprocess.run(arges, capture_output=True)
+    print(ret.stderr.decode("utf-8").replace("\r\n","\n"))
+    return ret.stdout.decode("utf-8").replace("\r\n", "\n")
+
+for i in listt:
+    for s in toMerge:
+        if re.match("^"+str(s)+"[^\\d]", i):
+            arges.append(i)
+
+print(arges)
+content = tryParse()
+print(content)
+open("./history/states/" + arges[4],"w+", encoding="utf-8").write(content)
+
+if True:
+    for f in arges[5:]:
+        os.remove("./history/states/" + f)
